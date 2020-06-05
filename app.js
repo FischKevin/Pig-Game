07e82328f1b73d0f440111f9a72903c6ec588b9a
Kévin FISCHER <kfischer@smag.tech>
2020-06-05 11:08:13 +0200

    fixed event on btn-hold + nextPlayer function

--- app.js
+++ app.js
@@ -14,43 +14,40 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
     let dice = Math.floor(Math.random() * 6) + 1;
     let diceDom = document.querySelector('.dice');
+    console.log(activePlayer);
     diceDom.style.display = 'block';
     diceDom.src = '/img/dice/dice-' + dice + '.png';
+    console.log(dice);
     // if dice value != 1 -> add dice value to round score
     if (dice != 1) {
         roundScore += dice; 
-        activePlayer = activePlayer + 1;
         document.querySelector('.score-round-' + activePlayer).textContent = roundScore;
-        activePlayer = activePlayer - 1;
     // else round value = 0 and next player
     } else {
-        roundScore = 0;
-        activePlayer = activePlayer + 1;
-        document.querySelector('.score-round-' + activePlayer).textContent = roundScore;
-        activePlayer = activePlayer - 1;
         nextPlayer();
     }
 });
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    if (activePlayer === 0) {
-        document.getElementById('player-1').classList.remove('active');
-        document.getElementById('player-2').classList.add('active');
-    } else {
-        document.getElementById('player-2').classList.remove('active');
-        document.getElementById('player-1').classList.add('active');
-    }
+    console.log(activePlayer);
+    roundScore = 0;
+    document.querySelector('.score-round-0').textContent = '0';
+    document.querySelector('.score-round-1').textContent = '0';
+
+    document.querySelector('#player-0').classList.toggle('active');
+    document.querySelector('#player-1').classList.toggle('active');
+
 }
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     // clicking btn-hold round score become total score and nextPlayer
-    /*activePlayer = activePlayer + 1;
-    document.querySelector('.score-total-' + activePlayer).textContent = scores[activePlayer];
-    activePlayer = activePlayer - 1;
-    document.querySelector('.score-round-' + activePlayer).textContent = scores[activePlayer];*/
-    nextPlayer();
 
+    scores[activePlayer] += roundScore;
+    document.querySelector('.score-total-' + activePlayer).textContent = scores[activePlayer];
+
+    //roundScore = 0;
+    nextPlayer();
 });
 
 document.querySelector('.btn-new').addEventListener('click', function() {
@@ -64,8 +61,10 @@ function init() {
     maxScore = 100;
 
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.score-total-0').textContent = '0';
     document.querySelector('.score-total-1').textContent = '0';
-    document.querySelector('.score-total-2').textContent = '0';
+    document.querySelector('.score-round-0').textContent = '0';
     document.querySelector('.score-round-1').textContent = '0';
-    document.querySelector('.score-round-2').textContent = '0';
+    document.getElementById('player-1').classList.remove('active');
+    document.getElementById('player-0').classList.add('active');
 }
